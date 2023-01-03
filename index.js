5e5c0cd56ef5ebc20682ec3f9d566672bc01c093
Digambaran <arjunsreekumar.1@gmail.com>
2023-01-03 21:53:27 +0530

    more block code

--- index.js
+++ index.js
@@ -1,17 +1,85 @@
+import { shared, env } from "@appblocks/node-sdk";
 
+env.init();
 
+const generateOTP = () => {
+  const email_verification_code = String(
+    Math.floor(100000 + Math.random() * 900000)
+  );
+  const email_verification_expiry = new Date();
+  email_verification_expiry.setMinutes(
+    email_verification_expiry.getMinutes() +
+      process.env.email_verification_expiry || 10
+  );
+  return { email_verification_code, email_verification_expiry };
+};
+
+function sendEmailVerificationCode({ email, email_verification_code }) {
+  return sendMail({
+    emailTo: email,
+    Subject: "verification code for your account",
+    Text: email_verification_code,
+    Html: email_verification_code,
+  });
+}
 const sample_shield_otp_resend_fn = async (req, res) => {
+  const { prisma, getBody, sendResponse } = await shared.getShared();
 
   // health check
   if (req.params["health"] === "health") {
-    res.write(JSON.stringify({success: true, msg: "Health check success"}))
-    res.end()
+    sendResponse(res, 200, { success: true, msg: "Health check success" });
+    return;
   }
 
-  // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Hello sample_shield_otp_resend_fn`}))
-  res.end()
-  
-}
+  const { email } = await getBody(req);
+  try {
+    const userData = await prisma.users.findFirst({ where: { email } });
+    if (!userData) {
+      sendResponse(res, 404, {
+        err: true,
+        msg: "email not found",
+        data: {},
+      });
+      return;
+    }
 
-export default sample_shield_otp_resend_fn
+    // generate new OTP
+    const { email_verification_code, email_verification_expiry } =
+      generateOTP();
+
+    const userDataPayload = await prisma.users.update({
+      where: { email },
+      data: {
+        email_verified: false,
+        email_verification_code,
+        email_verification_expiry,
+      },
+    });
+    console.log("userdatapaylodsd", userDataPayload);
+    if (!(await sendEmailVerificationCode(email, email_verification_code))) {
+      sendResponse(res, 500, {
+        err: true,
+        msg: "server error",
+        data: {},
+      });
+      return;
+    }
+
+    sendResponse(res, 200, {
+      err: true,
+      msg: "successfully geenrated new otp",
+      data: {},
+    });
+    return;
+  } catch (error) {
+    console.log(error);
+    sendResponse(res, 500, {
+      err: true,
+      msg: "server error",
+      data: {},
+    });
+    return;
+  }
+};
+
+export default sample_shield_otp_resend_fn;

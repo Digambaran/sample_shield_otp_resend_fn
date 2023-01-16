bc17601951fc6ddf1b2fb32a09b55e6a46c64a58
Digambaran <arjunsreekumar.1@gmail.com>
2023-01-16 20:56:09 +0530

    fixes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 #sample_shield_otp_resend_fn
+
+## env
+
+`EMAIL_VERIFICATION_EXPIRY` for OTP expiry (in mins)
+
+`SHIELD_MAILER_EMAIL` nodemailer from email
+
+`SHIELD_MAILER_PASSWORD` nodemailer password
+
+`SHIELD_MAILER_HOST` nodemailer host
+
+`SHIELD_MAILER_PORT` nodemailer port
--- index.js
+++ index.js
@@ -1,27 +1,53 @@
 import { shared, env } from "@appblocks/node-sdk";
+import { createTransport } from "nodemailer";
 
 env.init();
 
+const BLOCK_NAME = "SAMPLE_SHIELD_OTP_RESEND_FN";
+const getFromBlockEnv = (name) => process.env[BLOCK_NAME.toLocaleUpperCase() + "_" + name];
+
 const generateOTP = () => {
-  const email_verification_code = String(
-    Math.floor(100000 + Math.random() * 900000)
-  );
+  const email_verification_code = String(Math.floor(100000 + Math.random() * 900000));
   const email_verification_expiry = new Date();
   email_verification_expiry.setMinutes(
-    email_verification_expiry.getMinutes() +
-      process.env.email_verification_expiry || 10
+    email_verification_expiry.getMinutes() + getFromBlockEnv("EMAIL_VERIFICATION_EXPIRY") || 10
   );
   return { email_verification_code, email_verification_expiry };
 };
 
-function sendEmailVerificationCode({ email, email_verification_code }) {
-  return sendMail({
-    emailTo: email,
-    Subject: "verification code for your account",
-    Text: email_verification_code,
-    Html: email_verification_code,
+async function sendMail({ to, subject, text, html }) {
+  const from = getFromBlockEnv("SHIELD_MAILER_EMAIL");
+  const password = getFromBlockEnv("SHIELD_MAILER_PASSWORD");
+  const host = getFromBlockEnv("SHIELD_MAILER_HOST");
+  const port = getFromBlockEnv("SHIELD_MAILER_PORT");
+
+  const transporter = createTransport({
+    host,
+    port,
+    secure: port === 465, // true for 465, false for other ports
+    auth: { user: from, pass: password },
   });
+
+  console.log(`to:${to}`);
+  console.log(`from:${from}`);
+  console.log(`html:${html}`);
+  console.log(`text:${text}`);
+  console.log(`subject:${subject}`);
+
+  const info = await transporter.sendMail({
+    from,
+    to,
+    subject,
+    text,
+    html,
+  });
+
+  console.log("info");
+  console.log(info);
+  if (!info) throw new Error("Email not sent");
+  return info;
 }
+
 const sample_shield_otp_resend_fn = async (req, res) => {
   const { prisma, getBody, sendResponse } = await shared.getShared();
 
@@ -34,20 +60,20 @@ const sample_shield_otp_resend_fn = async (req, res) => {
   const { email } = await getBody(req);
   try {
     const userData = await prisma.users.findFirst({ where: { email } });
-    if (!userData) {
-      sendResponse(res, 404, {
+    if (!userData || userData.email_verified) {
+      // for security
+      sendResponse(res, 200, {
         err: true,
-        msg: "email not found",
+        msg: "successfully geenrated new otp",
         data: {},
       });
       return;
     }
 
     // generate new OTP
-    const { email_verification_code, email_verification_expiry } =
-      generateOTP();
+    const { email_verification_code, email_verification_expiry } = generateOTP();
 
-    const userDataPayload = await prisma.users.update({
+    await prisma.users.update({
       where: { email },
       data: {
         email_verified: false,
@@ -55,8 +81,16 @@ const sample_shield_otp_resend_fn = async (req, res) => {
         email_verification_expiry,
       },
     });
-    console.log("userdatapaylodsd", userDataPayload);
-    if (!(await sendEmailVerificationCode(email, email_verification_code))) {
+
+    try {
+      await sendMail({
+        to: email,
+        subject: "verification code for your account",
+        text: email_verification_code,
+        html: email_verification_code,
+      });
+    } catch (error) {
+      console.log(error);
       sendResponse(res, 500, {
         err: true,
         msg: "server error",
